c24e6b2c4b302c8056ae7d93f9e9c98734ad563a
Jesus David Sanchez <jesusdsm1703@gmail.com>
2026-03-18 19:47:57 -0400

    Se ha restructurado el contenido y añadido nuevas actualizaciones

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ document.addEventListener('DOMContentLoaded', () =>{
 	const gitHubButton = document.getElementById('github-button');
 
 	darkModeButton.addEventListener('click', ()=>{
-		if (stylesheet.getAttribute('href') === 'light-mode-stylesheet.css' ) {
-			stylesheet.setAttribute('href', 'dark-mode-stylesheet.css');
+		if (stylesheet.getAttribute('href') === 'styles/light-mode-stylesheet.css' ) {
+			stylesheet.setAttribute('href', 'styles/dark-mode-stylesheet.css');
 			titleButton.style.setProperty('font-weight', 'bold');
 			titleButton.textContent = "Light Mode";
 			iconButton.classList.replace('fa-moon', 'fa-sun');
 		}
 
 		else{
-			stylesheet.setAttribute('href', 'light-mode-stylesheet.css');
+			stylesheet.setAttribute('href', 'styles/light-mode-stylesheet.css');
 			titleButton.textContent = "Dark Mode";
 			iconButton.classList.replace('fa-sun','fa-moon');
 		}
@@ -22,6 +22,26 @@ document.addEventListener('DOMContentLoaded', () =>{
 
 	gitHubButton.addEventListener('click', () =>{
 		window.open('https://github.com/Jesanchezz?tab=repositories', '_blank');
+	});
+
+	//Animacion de desplazamiento de izquerda a derecha
+	const sections = document.querySelectorAll('.section-container');
+	const observerOptions={
+		root:null,
+		threshold: 0.1
+	};
+
+	const observer = new IntersectionObserver((entries, observer) =>{
+		entries.forEach(entry =>{
+			if (entry.isIntersecting){
+				entry.target.classList.add('visible');
+				observer.unobserve(entry.target);
+			}
+		});
+	}, observerOptions);
+	
+	sections.forEach(section =>{
+		observer.observe(section);
 	});
 });
 
